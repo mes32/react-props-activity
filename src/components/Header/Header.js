383d73b58086116fdd3bca205cc795870a949f7c
mes32 <stockman.mike@gmail.com>
2019-01-16 15:11:26 -0600

    Add class/component named Header.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="header">
+                React Components
+            </header>
+        );
+    }
+}
+
+export default Header;
